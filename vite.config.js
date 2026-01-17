41400a4ed0588913957d6e169ad3399a22e3b670
Kartik Marathe <orian.neo007@gmail.com>
2026-01-17 10:32:42 -0700

    update base path in vite config for custom domain root

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/animated_portfolio/", // Replace with your actual repository name
+  base: "/", // Custom domain root
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
